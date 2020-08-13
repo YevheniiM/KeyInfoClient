86f4fde311b8d946321a11c683f0eeeb603fcedb
YevheniiM <xmolodtsov@gmail.com>
2020-08-13 22:22:45 +0300

    Add docs

--- common/analyzer.py
+++ common/analyzer.py
@@ -1,15 +1,22 @@
+"""The module contains the logic of analyzing the
+keyboard events."""
+
 import sys
 
 from keyboard import KEY_DOWN, KeyboardEvent
 
 
 class KeyboardEventAnalyzer:
+    """The class do the main logic of a program"""
+
     MAX_BUFFER_LENGTH = 10000
 
     def __init__(self):
         self.events = []
 
     def analyze(self, event: KeyboardEvent):
+        """Analyzes the event and flushes the buffer if needed."""
+
         if not self.__check_max_buffer():
             self.__flush_buffer()
         self._print_event(event)
@@ -17,10 +24,13 @@ class KeyboardEventAnalyzer:
 
     @staticmethod
     def _print_event(event: KeyboardEvent):
+        """Prints the key when it is pressed (only on KEY_DOWN action)."""
+
         if event.event_type == KEY_DOWN:
             sys.stdout.write(f"The [{event.name}] key was pressed ({event.event_type})")
 
     def __check_max_buffer(self):
+        """Checks the buffer length."""
         return len(self.events) < KeyboardEventAnalyzer.MAX_BUFFER_LENGTH
 
     def __flush_buffer(self):
--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import sys
 
 import keyboard
+from keyboard import KeyboardEvent
 
 from common.analyzer import KeyboardEventAnalyzer
 
 analyzer = KeyboardEventAnalyzer()
 
 
-def analyze(event):
+def analyze(event: KeyboardEvent):
+    """The function calls each time the key is pressed and
+    make some analysis."""
+
     analyzer.analyze(event)
     sys.stdout.flush()
 
 
 def main():
+    """The entry point of a program"""
+
     keyboard.hook(analyze)
     keyboard.wait()
 
